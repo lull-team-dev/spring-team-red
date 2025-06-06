384766fe213e69a422780b824d4a89e0ae1dcf64
門馬一樹 <monmakazuki@monmakazukinoMacBook-Air.local>
2025-06-06 22:26:49 +0900

    hotelエンティティ作成

---
+++ src/main/java/com/example/demo/entity/Hotel.java
@@ -0,0 +1,98 @@
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "hotels")
+public class Hotel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+	private String photo;
+	private String prefecture;
+	private String city;
+
+	
+	//plansテーブルとのリレーション
+	@OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL)
+	private List<Plan> plans;
+
+	//reservationテーブルとのリレーション
+	@OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL)
+	private List<Reservation> reservations;
+
+	public Hotel() {
+	}
+
+	public Hotel(Integer id, String name, String photo, String prefecture, String city) {
+		this.id = id;
+		this.name = name;
+		this.photo = photo;
+		this.prefecture = prefecture;
+		this.city = city;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public String getPrefecture() {
+		return prefecture;
+	}
+
+	public void setPrefecture(String prefecture) {
+		this.prefecture = prefecture;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public List<Plan> getPlans() {
+		return plans;
+	}
+
+	public void setPlans(List<Plan> plans) {
+		this.plans = plans;
+	}
+
+	public List<Reservation> getReservations() {
+		return reservations;
+	}
+
+	public void setReservations(List<Reservation> reservations) {
+		this.reservations = reservations;
+	}
+}
