ee3b9e90df62822a9ab85e419747d27512f908c0
417MAIKA_PC\k017a <may09tosi@gmail.com>
2025-06-07 19:47:38 +0900

    0607修正依頼の対応をしましたhttps://github.com/lull-team-dev/spring-team-red/pull/12#issuecomment-2952286732

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,10 +1,15 @@
 package com.example.demo.entity;
 
+import java.util.ArrayList;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "users") //usersテーブルを指定
@@ -24,19 +29,24 @@ public class User {
 
 	private String password; //パスワード
 
-	private String confirmPassword; //パスワード確認用
+	@Transient
+	private String confirmPassword; //パスワード確認用(DBに保存されない（確認用フィールド）)
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<Booking> posts = new ArrayList<>();
 
 	//コンストラクタ
 
 	public User() {
 	}
 
-	public User(String name, String address, String tel, String email, String password) {
+	public User(String name, String address, String tel, String email, String password, String confirmPassword) {
 		this.name = name;
 		this.address = address;
 		this.tel = tel;
 		this.email = email;
 		this.password = password;
+		this.confirmPassword = confirmPassword;
 	}
 
 	//ゲッター
@@ -66,6 +76,35 @@ public class User {
 
 	public String getConfirmPassword() {
 		return confirmPassword;
+	}
+
+	//セッター
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setContirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
 	}
 
 }
