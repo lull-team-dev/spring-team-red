b203c6e1a8271737449f25dcd99a4063a3a6e022
tomohito shimazu <xa12-86703>
2025-06-28 16:39:53 +0900

    integerに変更

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 	List<User> findByEmailAndPassword(String email, String password);
 
 	boolean existsByEmail(String email);
