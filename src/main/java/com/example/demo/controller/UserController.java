42d6d20ea939cef8abeda56640d9e3fc72065052
tomohito shimazu <xa12-86703>
2025-06-28 19:56:28 +0900

    マイページエラー処理

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -35,15 +35,15 @@ public class UserController {
 	@GetMapping("/mypage/{id}")
 	public String index(
 			@PathVariable("id") Integer id,
-			@RequestParam ("lastName") String lastName,
-			@RequestParam ("firstName") String firstName,
-			@RequestParam ("address") String address,
-			@RequestParam ("tel") String tel,
-			@RequestParam ("email") String email,
+			@RequestParam (value = "lastName",required = false) String lastName,
+			@RequestParam (value = "firstName",required = false) String firstName,
+			@RequestParam (name = "address",required = false) String address,
+			@RequestParam (name = "tel",required = false) String tel,
+			@RequestParam (name = "email",required = false) String email,
 			Model model) {
 		
-		User user = userRepository.findById(id).get();
-		Reservation reservation =reservationRepository.findById(id).get();
+		User user = userRepository.findById(id).orElse(null);
+		Reservation reservation =reservationRepository.findById(id).orElse(null);
 		
 		model.addAttribute("user",user);
 		model.addAttribute("reservation",reservation);
@@ -56,19 +56,20 @@ public class UserController {
 	@GetMapping("/mypage/{id}/edit")
 	public String edit(
 			@PathVariable("id") Integer id,
-			@RequestParam ("lastName") String lastName,
-			@RequestParam ("firstName") String firstName,
-			@RequestParam ("address") String address,
-			@RequestParam ("tel") String tel,
-			@RequestParam ("email") String email,
-			@RequestParam ("password") String password,
+			@RequestParam (value = "lastName",required = false) String lastName,
+			@RequestParam (value = "firstName",required = false) String firstName,
+			@RequestParam (name = "address",required = false) String address,
+			@RequestParam (name = "tel",required = false) String tel,
+			@RequestParam (name = "email",required = false) String email,
+			@RequestParam (name="password",required = false) String password,
+			@RequestParam(name="confirmPassword",required = false) String confirmPassword,
 			Model model) {
 		
 		User user = userRepository.findById(id).get();
 		
 		model.addAttribute("user",user);
 		
-		return "edit";
+		return "userEdit";
 	}
 	
 	//ユーザー情報更新（編集）
@@ -83,9 +84,11 @@ public class UserController {
 			@RequestParam (name = "tel",defaultValue = "") String tel,
 			@RequestParam (name = "email",defaultValue = "") String email,
 			@RequestParam (name = "password",defaultValue = "") String password,
+			@RequestParam(name="confirmPassword",defaultValue = "") String confirmPassword,
+
 			Model model){
 		
-		User user = userRepository.findById(id).get();
+		User user = userRepository.findById(id).orElse(null);
 		
 //		エラー処理
 		List<String> errorList = new ArrayList<>();
@@ -102,11 +105,11 @@ public class UserController {
 		if(tel.isEmpty()) {
 			errorList.add("電話番号は必須です");
 		}
-		if(email.isEmpty()) {
-			errorList.add("メールアドレスは必須です");
-		}else if(!email.contains("@")) { //指定した値が入っているか
-		    errorList.add("正しいメールアドレスを入力してください（@が必要です）");
-		}
+//		if(email.isEmpty()) {
+//			errorList.add("メールアドレスは必須です");
+//		}else if(!email.contains("@")) { //指定した値が入っているか
+//		    errorList.add("正しいメールアドレスを入力してください（@が必要です）");
+//		}
 		
 		if(password.isEmpty()) {
 			errorList.add("パスワードは必須です");
@@ -126,6 +129,8 @@ public class UserController {
 		user.setTel(tel);
 		user.setEmail(email);
 		user.setPassword(password);
+		user.setConfirmPassword(confirmPassword); // ここが超重要！
+
 	
 		userRepository.save(user);
 	
