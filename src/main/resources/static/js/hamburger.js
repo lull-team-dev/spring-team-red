3e7aa0543242dc50b27387fba9c19fc547cbacc9
門馬一樹 <monmakazuki@monmakazukinoMacBook-Air.local>
2025-07-13 16:01:39 +0900

    ヘッダー及びフッターの修正

--- src/main/resources/static/js/hamburger.js
+++ src/main/resources/static/js/hamburger.js
@@ -1,20 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const sidebar = document.getElementById("sidebar");
+  const overlay = document.getElementById("overlay");
 
-			const hamburger = document.getElementById('hamburger');
-			const sidebar = document.getElementById('sidebar');
-			const overlay = document.getElementById('overlay');
-			
-            if (!hamburger || !sidebar || !overlay) return;
+  hamburger.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    hamburger.classList.toggle("active");
+  });
 
-			hamburger.addEventListener('click', () => {
-				hamburger.classList.toggle('active');
-				sidebar.classList.toggle('active');
-				overlay.classList.toggle('active');
-			});
-			
-			overlay.addEventListener('click', () => {
-				hamburger.classList.remove('active');
-				sidebar.classList.remove('active');
-				overlay.classList.remove('active');
-			});
-		});
+  overlay.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+    overlay.classList.remove("active");
+    hamburger.classList.remove("active");
+  });
+});
